e508b53cf7a3c7c0265a8de723f258517f03a621
hungtabe <trandinhhung717@gmail.com>
2025-07-06 09:40:03 +0700

    [br_1] Base project

---
+++ PRN222_PE_TranDinhHung/MovieWeb_BO/Entities/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieWeb_BO.Entities;
+
+public partial class Genre
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public virtual ICollection<Movie> Movies { get; set; } = new List<Movie>();
+}
